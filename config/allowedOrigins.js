860c63c5197d5c42e4750be18b60f4959d528a2f
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2022-12-29 13:31:11 +0700

    first commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://www.dandrepairshop.com",
-  "https://dandrepairshop.com",
+  "https://thaytungdaylaixe.github.io/yourapp/",
 ];
 export default allowedOrigins;
